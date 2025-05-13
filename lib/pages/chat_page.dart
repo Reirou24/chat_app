feb291026935eb643138f362d3df00ac5ba082f2
Ferdinand II Dangaran <2091-22@itmlyceumalabang.onmicrosoft.com>
2025-05-13 15:23:06 +0800

    fixed chatting

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,5 +1,7 @@
+import 'package:chat_app/components/textfield_widget.dart';
 import 'package:chat_app/services/auth/auth_service.dart';
 import 'package:chat_app/services/chat/chat_services.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatelessWidget {
@@ -32,6 +34,58 @@ class ChatPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(receiverEmail),
       ),
+      body: Column(
+        children: [
+          Expanded(
+            child: _buildMessageList(),
+          ),
+
+          _buildUserInput(),
+          
+        ],
+      )
+    );
+  }
+
+  Widget _buildMessageList() {
+    String senderID = _authService.getCurrentUser()!.uid;
+    return StreamBuilder(
+      stream: _chatServices.getMessaages(receiverID, senderID), 
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return const Text("Error");
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text("Loading...");
+        }
+
+        return ListView(
+          children: snapshot.data!.docs.map((doc) => _buildMessageItem(doc)).toList(),
+        );
+      },
+    );
+  }
+
+  Widget _buildMessageItem(DocumentSnapshot doc) {
+    Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+
+    return Text(data["message"]);
+  }
+
+  Widget _buildUserInput() {
+    return Row(
+      children: [
+        Expanded(
+          child: TextfieldWidget(
+            controller: _messageController,
+            hintText: "Type a message", 
+            hideText: false, 
+            ),
+        ),
+
+        IconButton(onPressed: sendMessage, icon: Icon(Icons.arrow_upward))
+      ],
     );
   }
 }
